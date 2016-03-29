4a0438666aaa388561e832472f397d5cc026c1ea
gj <67007555@qq.com>
2016-03-29 14:20:26 +0800

    one question with

--- src/js/one.js
+++ src/js/one.js
@@ -1,13 +1,30 @@
 /**
  * [foo description] one question 
- * @type {Object} return undefined
+ * @type {Object}
  */
-var foo = {
-	bar: function() {
-		return this.baz;
-	},
-	baz: 1
-};
-(function() {
-	return typeof arguments[0]();
-})(foo.bar);
+var o = {
+	x: 10,
+	foo: function() {
+		//A
+		//B
+		with(this) {
+			//此处的函数声明 bar 因为是函数声明语句的关系，其声明会被提升到函数的顶部,所以与放在A处相同
+			function bar() {
+				console.log(x);
+				console.log(this.x);
+			}
+
+			var x = 20; //也是涉及变量声明提升的问题，B处的代码为var x;此处的等价代码为x=20;因为with语句的关系，变为对this中的属性x赋值          
+			(function() {
+				bar(); //此时bar匿名函数外层函数foo中定义的函数对象，此时this为全局对象               
+			})();
+			bar.call(this); ////此时bar匿名函数外层函数foo中定义的函数对象 ，此时this为传入的对象o    
+		}
+
+		console.log(this.x);
+		console.log(this.bar);
+
+
+	}
+}
+o.foo();
